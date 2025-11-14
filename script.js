baca38c0b0e5252f782736b833c9685a346645cd
lucariggi92 <riggiluca92@gmail.com>
2025-11-14 17:22:02 +0100

    feat: print in html 1 email

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const apiUrl = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
-
+    const listElem = document.querySelector("ul")
 //chiamare API del server per generare una mail random
 axios
 .get("https://flynn.boolean.careers/exercises/api/random/mail").then(function (resp){
 
-    console.log("risposta arrivata", resp.data.response);
+    const randomEmail =resp.data.response;
+listElem.innerHTML = `<li>${randomEmail}</li>`
+
+
+    
 });
