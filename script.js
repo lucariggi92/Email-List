b4967de9f033efbc44d6c0220f14ec6bf54c641c
lucariggi92 <riggiluca92@gmail.com>
2025-11-14 17:51:39 +0100

    feat: stampate tutte e 10 le mail

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const apiUrl = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
-    const listElem = document.querySelector("ul")
+const listElem = document.querySelector("ul")
+const totale = 10;
+
+let emailTotali = "";
 //chiamare API del server per generare una mail random
-axios
-.get("https://flynn.boolean.careers/exercises/api/random/mail").then(function (resp){
 
-    const randomEmail =resp.data.response;
-listElem.innerHTML = `<li>${randomEmail}</li>`
+for (i = 0; i < totale; i++) {
 
 
-    
-});
+    axios
+        .get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(function (resp) {
+
+            const randomEmail = resp.data.response;
+
+            emailTotali = emailTotali + `<li>${randomEmail}</li>`
+
+            listElem.innerHTML = emailTotali
+
+   
+
+
+        });
+                 
+}
