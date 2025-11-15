da4c5d545dbf39053f8288254e296cc333a3a0ab
lucariggi92 <riggiluca92@gmail.com>
2025-11-15 15:20:47 +0100

    feat: button email List generate completed

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 const apiUrl = "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
 const listElem = document.querySelector("ul")
+const btnGenera = document.querySelector("#btn-genera")
 const totale = 10;
 
-let emailTotali = "";
+
 //chiamare API del server per generare una mail random
 
-for (i = 0; i < totale; i++) {
+function generaListaemail() {
+    let emailTotali = "";
+    listElem.innerHTML = "";
+    const emailArray =[];
+
+    for (let i = 0; i < totale; i++) {
 
 
-    axios
-        .get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(function (resp) {
+        axios
+            .get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(function (resp) {
 
-            const randomEmail = resp.data.response;
+                const randomEmail = resp.data.response;
 
-            emailTotali = emailTotali + `<li>${randomEmail}</li>`
+                emailTotali = emailTotali + `<li>${randomEmail}</li>`
 
-            listElem.innerHTML = emailTotali
-
-   
+                emailArray.push(randomEmail)
 
 
-        });
-                 
+                if (emailArray.length === totale) {
+                    listElem.innerHTML = emailTotali
+                }
+
+            });
+
+    }
 }
+generaListaemail();
+
+
+btnGenera.addEventListener("click", function () {
+    generaListaemail();
+})
